079f38ba62d745dfe9d008dd34b174a9c3c92043
Khalid Uris <uriskhalid@gmail.com>
2024-01-02 05:19:34 +0500

    store product data.

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -3,7 +3,11 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\Product;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
 
 class ProductController extends Controller
 {
@@ -12,7 +16,15 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        $product = Product::all();
+        $category = Category::all();
+
+        return response()->json([
+            'status' => 1,
+            'category' => $category,
+            'product' => $product
+        ],200);
+        // $product=Product::with('category')->get();
     }
 
     /**
@@ -20,15 +32,53 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // first Method
+        // $category = Category::find($request->category_id);
+        // $product = $category->products()->create([
+        //     'name'=>$request->name,
+        //     'slug'=>Str::slug($request->slug),
+        //     'price' => $request->price,
+        // ]);
+
+        // second Method
+        try {
+        $category = Category::find($request->category_id);
+
+        $product = new Product();
+        $product->name = $request->name;
+        $product->slug = Str::slug($request->slug);
+        $product->price = $request->price;
+
+       $data = $category->products()->save($product);
+
+        return response()->json([
+            'status' => 1,
+            'data' => $data,
+
+        ],200);
+        } catch (Exception $e) {
+            return response()->json([
+                'status' => 0,
+                'error' => $e->getMessage(),
+
+            ],500);
+        }
+
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show()
     {
-        //
+         $product = Product::with('category')->get();
+        //$category = Category::with('products')->get();
+
+        return response()->json([
+            'status' => 1,
+            'data' => $product
+            //'data' => $category
+        ]);
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -23,4 +24,8 @@ use Illuminate\Support\Facades\Route;
 Route::group(['prefix'=>'admin'],function(){
     Route::post('/add/category',[CategoryController::class,'store']);
     Route::get('/get/category',[CategoryController::class,'index']);
+
+    Route::get('/products',[ProductController::class,'index']);
+    Route::post('/add/products',[ProductController::class,'store']);
+    Route::get('/products/list',[ProductController::class,'show']);
 });
